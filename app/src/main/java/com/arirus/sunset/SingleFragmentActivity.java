756df7e203758dc08d829d39fd3846167c3066e8
Arirus <whd910421@hotmail.com>
2016-10-31 12:26:41 +0800

    可以显示

--- app/src/main/java/com/arirus/sunset/SingleFragmentActivity.java
+++ app/src/main/java/com/arirus/sunset/SingleFragmentActivity.java
@@ -1,34 +1,38 @@
 package com.arirus.sunset;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.util.AttributeSet;
-import android.view.View;
+import android.support.v7.app.AppCompatActivity;
 
 /**
- * Created by whd910421 on 16/10/31.
+ * Created by whd910421 on 16/10/25.
  */
 
-public abstract class SingleFragmentActivity extends FragmentActivity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
     private static final String TAG = "SingleFragmentActivity";
-    private Fragment mFragment = null;
+    private Fragment mFragment ;
+
+    protected int getLayoutResID()
+    {
+        return R.layout.activity_fragment;
+    }
 
     protected abstract Fragment newFragment();
 
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.activity_fragment);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResID());
+
         FragmentManager fm = getSupportFragmentManager();
         mFragment = fm.findFragmentById(R.id.fragment_container);
+
         if (mFragment == null)
         {
-            mFragment = newFragment();
-            fm.beginTransaction().add(R.id.fragment_container , mFragment).commit();
+            mFragment= newFragment();
+            fm.beginTransaction().add(R.id.fragment_container, mFragment).commit();
         }
     }
 }
